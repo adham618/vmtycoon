baccffc0eb7c0c03dcf0aba69176ae72f4603394
adham618 <adhamtarek291@gmail.com>
2022-08-08 21:46:01 +0200

    create themetoggle components

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { BsFillSunFill, BsMoonStarsFill } from "react-icons/bs";
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+// import { BsFillSunFill, BsMoonStarsFill } from "react-icons/bs";
 
 export const useLoaded = () => {
   const [loaded, setLoaded] = useState(false);
@@ -9,19 +9,19 @@ export const useLoaded = () => {
 };
 
 export default function ThemeToggle() {
-  const loaded = useLoaded();
+  // const loaded = useLoaded();
   const { theme, setTheme } = useTheme();
   return (
     <button
-      aria-label="theme toggle"
-      className="border-none p-2 hover:rounded-full hover:bg-slate-200 focus:outline-none  dark:text-slate-200 dark:hover:bg-opacity-40"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      aria-label='theme toggle'
+      className='border-none p-2 hover:rounded-full hover:bg-slate-200 focus:outline-none  dark:text-slate-200 dark:hover:bg-opacity-40'
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {theme === "light" && loaded ? (
-        <BsMoonStarsFill size={16} />
+      {/* {theme === "light" && loaded ? (
+        // <BsMoonStarsFill size={16} />
       ) : (
-        <BsFillSunFill size={16} />
-      )}
+        // <BsFillSunFill size={16} />
+      )} */}
     </button>
   );
 }
