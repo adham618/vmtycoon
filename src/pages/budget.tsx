53a789bd48c0772390e1a3ba42aac5a2052ad8ed
adham618 <adhamtarek291@gmail.com>
2022-08-08 21:51:57 +0200

    create budget page

---
+++ src/pages/budget.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const BudgetPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Budget</title>
+      </Head>
+      <div>BudgetPage</div>
+    </>
+  );
+};
+
+export default BudgetPage;
