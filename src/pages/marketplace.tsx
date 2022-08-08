614b9f24553a2a974ce45f0a5846332023fb983a
adham618 <adhamtarek291@gmail.com>
2022-08-08 21:53:09 +0200

    create Marketplace Page

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -1,7 +1,15 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const MarketplacePage: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Marketplace</title>
+      </Head>
+      <div>MarketplacePage</div>
+    </>
+  );
 };
 
 export default MarketplacePage;
