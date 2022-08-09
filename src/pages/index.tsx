9d82b5c9b8e2d60426c7e3f3b4a1470d201f65dc
adham618 <adhamtarek291@gmail.com>
2022-08-09 12:56:45 +0200

    add the components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import Exchanges from '@/components/Exchanges';
+import Features from '@/components/Features';
 import Hero from '@/components/Hero';
+import Minted from '@/components/Minted';
+import Overview from '@/components/Overview';
+import Roadmap from '@/components/Roadmap';
+import Statistics from '@/components/Statistics';
+import Vision from '@/components/Vision';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +17,13 @@ const Home: NextPage = () => {
         <title>VMTycoon</title>
       </Head>
       <Hero />
+      <Statistics />
+      <Overview />
+      <Features />
+      <Minted />
+      <Vision />
+      <Roadmap />
+      <Exchanges />
     </>
   );
 };
