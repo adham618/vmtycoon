e080a506f1ba04d564eab58dec6d00afe96b16a4
adham618 <adhamtarek291@gmail.com>
2022-08-09 12:51:12 +0200

    finish themeToggle component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,5 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-// import { BsFillSunFill, BsMoonStarsFill } from "react-icons/bs";
 
 export const useLoaded = () => {
   const [loaded, setLoaded] = useState(false);
@@ -9,19 +8,33 @@ export const useLoaded = () => {
 };
 
 export default function ThemeToggle() {
-  // const loaded = useLoaded();
+  const loaded = useLoaded();
   const { theme, setTheme } = useTheme();
   return (
     <button
       aria-label='theme toggle'
-      className='border-none p-2 hover:rounded-full hover:bg-slate-200 focus:outline-none  dark:text-slate-200 dark:hover:bg-opacity-40'
+      className='border-none  focus:outline-none'
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {/* {theme === "light" && loaded ? (
-        // <BsMoonStarsFill size={16} />
+      {theme === 'light' && loaded ? (
+        <div className='relative z-50 m-2 flex items-center'>
+          <label
+            htmlFor='switch'
+            className='relative inline-block h-[35px] w-[35px]'
+          >
+            <span className="absolute right-0 left-0 bottom-0 top-0 cursor-pointer rounded-[50px] border border-solid border-[#e2e2e2] bg-white shadow-lg duration-[400ms] before:absolute before:left-[4px] before:bottom-[4px] before:h-[24px] before:w-[24px] before:rounded-[50%] before:bg-[#ffc400] before:bg-sun before:bg-center before:bg-no-repeat before:content-[''] dark:border-white/30 dark:bg-[#242424] dark:before:bg-blue-600 dark:before:bg-moon"></span>
+          </label>
+        </div>
       ) : (
-        // <BsFillSunFill size={16} />
-      )} */}
+        <div className='relative z-50 m-2 flex items-center'>
+          <label
+            htmlFor='switch'
+            className='relative inline-block h-[35px] w-[35px]'
+          >
+            <span className="absolute right-0 left-0 bottom-0 top-0 cursor-pointer rounded-[50px] border border-solid border-[#e2e2e2] bg-white shadow-lg duration-[400ms] before:absolute before:left-[4px] before:bottom-[4px] before:h-[24px] before:w-[24px] before:rounded-[50%] before:bg-[#ffc400] before:bg-sun before:bg-center before:bg-no-repeat before:content-[''] dark:border-white/30 dark:bg-[#242424] dark:before:bg-blue-600 dark:before:bg-moon"></span>
+          </label>
+        </div>
+      )}
     </button>
   );
 }
