650e41ba08e61775e15c2d13c9852f678b693cb8
adham618 <adhamtarek291@gmail.com>
2022-08-09 12:56:29 +0200

    remove meta from Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -16,10 +17,6 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          />
           {/* Google fonts */}
           <link
             href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;900&family=Poppins:wght@400;500;600;700;900&display=swap'
@@ -34,6 +31,13 @@ class MyDocument extends Document {
         <body className='relative overflow-x-clip scroll-smooth font-openSans text-slate-600 dark:text-gray-400'>
           <Main />
           <NextScript />
+          <Script
+            src='https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'
+            integrity='sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=='
+            crossOrigin='anonymous'
+            referrerPolicy='no-referrer'
+          ></Script>
+          <script>new WOW().init();</script>
         </body>
       </Html>
     );
