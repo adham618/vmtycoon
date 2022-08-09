ca97693ec4e3a45a9115ed6f880b279176cfea58
adham618 <adhamtarek291@gmail.com>
2022-08-09 15:12:22 +0200

    config wowjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { useEffect } from 'react';
 
 import '../styles/globals.css';
 
-import Layout from '../components/Layout';
-
+import Layout from '@/components/Layout';
+const isServer = typeof window === 'undefined';
+const WOW = !isServer ? require('wow.js') : null;
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    new WOW().init();
+  }, []);
   return (
     <ThemeProvider attribute='class' defaultTheme='system'>
       <Layout>
