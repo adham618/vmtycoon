144db140c9224cee3ade8570f1779b626bbf2ccd
adham618 <adhamtarek291@gmail.com>
2022-08-10 15:54:46 +0200

    create scrolltop component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,16 @@
-import * as React from "react";
+import * as React from 'react';
 
-import Footer from "./Footer";
-import Header from "./Header";
+import ScrollTop from '@/components/ScrollTop';
+
+import Footer from './Footer';
+import Header from './Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       {children}
+      <ScrollTop />
       <Footer />
     </>
   );
---
+++ src/components/ScrollTop/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export default function ScrollTop() {
+  const [onTop, setOnTop] = React.useState(true);
+  const handleScroll = () => {
+    if (onTop !== window.pageYOffset < 600) {
+      setOnTop(window.pageYOffset < 600);
+    }
+  };
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+  return (
+    <a
+      href='#'
+      id='scrollTop'
+      className={`${
+        onTop ? '' : 'show'
+      } fixed -right-96 bottom-8 z-[9999] flex h-[50px] w-[30px] rounded-full bg-[#00baff] bg-arrow-up bg-center bg-no-repeat opacity-40 transition-all duration-300 hover:text-white hover:opacity-100`}
+      style={{ backgroundSize: '15px;' }}
+    ></a>
+  );
+}
