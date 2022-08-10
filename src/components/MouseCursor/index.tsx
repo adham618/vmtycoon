46be98676a1927f929e7bc91d9c927083d8f3f26
adham618 <adhamtarek291@gmail.com>
2022-08-10 16:26:31 +0200

    config mouse cursor

--- src/components/MouseCursor/index.tsx
+++ src/components/MouseCursor/index.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
 
 export default function MouseCursor() {
+  const [x, setX] = React.useState(0);
+  const [y, setY] = React.useState(0);
+  React.useLayoutEffect(() => {
+    document.onmousemove = (event) => {
+      const e = event;
+      setX(e.clientX);
+      setY(e.clientY);
+    };
+  });
+  const styles = {
+    transform: `translate(${x}px, ${y}px)`,
+  };
   return (
     <>
-      <div className='cursor point pointer-events-none fixed left-0 top-0 z-[9999] -ml-1 -mt-1 hidden h-2 w-2 rounded-[50%] bg-cursor-gradient opacity-90 transition-all duration-75 ease-in-out lg:block'></div>
-      <div className='cursor cursor-border pointer-events-none fixed left-0 top-0 z-[9999] -ml-[18px] -mt-[19px] hidden h-[38px] w-[38px] rounded-[50%] border border-cursor opacity-90 transition-all duration-75 ease-in-out lg:block'></div>
+      <div
+        style={styles}
+        className='cursor point pointer-events-none fixed left-0 top-0 z-[9999] -ml-1 -mt-1 hidden h-2 w-2 rounded-[50%] bg-cursor-gradient opacity-90 transition-all duration-75 ease-in-out lg:block'
+      ></div>
+      <div
+        style={styles}
+        className='cursor cursor-border pointer-events-none fixed left-0 top-0 z-[9999] -ml-[18px] -mt-[19px] hidden h-[38px] w-[38px] rounded-[50%] border border-cursor opacity-90 transition-all duration-75 ease-in-out lg:block'
+      ></div>
     </>
   );
 }
